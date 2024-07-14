15040345c3af48cf65b15337e5565dbd0abb2269
vadai gautam <vadaigautam0@.in>
2024-07-14 09:18:49 -0700

    convert_string_list

--- basic_pratice/num_convert_into_list_tuple.py
+++ basic_pratice/num_convert_into_list_tuple.py
@@ -1,27 +1,13 @@
 '''Write a Python program that accepts a sequence of comma-separated numbers from the user and generates a list and a tuple of those numbers.'''
 
 
-# user_input = input("Enter the number and separated by commas")
+user_input = input("Enter the number and separated by commas")
 
-# print(user_input)
+print(user_input)
 
-# lisst = user_input.split(" , ")
+lisst = user_input.split(",")
 
-# tuplee = tuple(lisst)
+tuplee = tuple(lisst)
 
-# print(f"In the list format = {lisst} \nIn the tuple format = {tuplee}")
+print(f"list : {lisst}")
 
-# Prompt the user to input a sequence of comma-separated numbers and store it in the 'values' variable
-values = input("Input some comma-separated numbers: ")
-
-# Split the 'values' string into a list using commas as separators and store it in the 'list' variable
-list = values.split(",")
-
-# Convert the 'list' into a tuple and store it in the 'tuple' variable
-tuple = tuple(list)
-
-# Print the list
-print('List : ', list)
-
-# Print the tuple
-print('Tuple : ', tuple)
